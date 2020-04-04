dc6aa4e6560d59856f30b75d6f7cc0380af63763
tim1125 <tim11251994@gmail.com>
2020-04-04 14:39:44 +0800

    Update UCL_SceneSwitcher

--- UCL_SceneScript/UCL_SceneSwitcher.cs
+++ UCL_SceneScript/UCL_SceneSwitcher.cs
@@ -56,46 +56,12 @@ namespace UCL.SceneLib {
     public class UCL_SceneSwitcher : ScriptableObject {
         [System.Serializable]
         public struct SceneData {
-            //public string m_SceneName;// = "Assets/Scenes/";
-            //public string m_Path;
-            public string m_IconName;
             public Object m_Scene;
-            /*
-            public bool UpdateData() {
-                bool altered = false;
-#if UNITY_EDITOR
-                if(m_Scene == null) {
-                    return false;
-                }
-                
-                if(m_SceneName != m_Scene.name) {
-                    m_SceneName = m_Scene.name;
-                    altered = true;
-                }
-                var path = AssetDatabase.GetAssetPath(m_Scene.GetInstanceID());
-                if(path != m_Path) {
-                    m_Path = path;
-                    altered = true;
-                }
-#endif
-                return altered;
-            }
-            */
+
             public string GetSceneName() {
                 if(m_Scene == null) return "";
 
                 return m_Scene.name;
-            }
-            public string GetIconName() {
-                if(m_Scene == null) return "";
-
-                string m_SceneName = m_Scene.name;
-                if(string.IsNullOrEmpty(m_IconName)) {
-                    int len = m_SceneName.Length;
-                    if(len > 2) len = 2;
-                    return m_SceneName.Substring(0, len);
-                }
-                return m_IconName;
             }
             public string GetPath() {
                 if(m_Scene == null) return "";
