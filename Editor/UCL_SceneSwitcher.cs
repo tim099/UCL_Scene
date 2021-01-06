422aa63219540e6da6260d8ff890771d158d6fbe
tim099 <tim11251994@gmail.com>
2021-01-06 17:39:51 +0800

    Add UCL_SceneScript/UCL_EditorSceneLoader.cs

--- Editor/UCL_SceneSwitcher.cs
+++ Editor/UCL_SceneSwitcher.cs
@@ -6,54 +6,6 @@ using UnityEngine;
 
 namespace UCL.SceneLib {
 
-
-#if UNITY_EDITOR
-    #region Editor
-    public static class EditorSceneLoader {
-        static string m_SceneToOpen;
-        static bool m_Rigistered = false;
-        public static void LoadScene(string scene) {
-            if(UnityEditor.EditorApplication.isPlaying) {
-                UnityEditor.EditorApplication.isPlaying = false;
-            }
-
-            m_SceneToOpen = scene;
-            if(!m_Rigistered) {
-                UnityEditor.EditorApplication.update += OnUpdate;
-                m_Rigistered = true;
-            } else {
-                Debug.LogWarning("SceneHelper m_Rigistered!!");
-            }
-
-        }
-
-        static void OnUpdate() {
-            if(m_SceneToOpen == null) {
-                UnityEditor.EditorApplication.update -= OnUpdate;
-                m_Rigistered = false;
-                return;
-            }
-
-            if(UnityEditor.EditorApplication.isPlaying || UnityEditor.EditorApplication.isPaused ||
-                UnityEditor.EditorApplication.isCompiling || UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode) {
-                return;
-            }
-
-            UnityEditor.EditorApplication.update -= OnUpdate;
-            m_Rigistered = false;
-
-            if(UnityEditor.SceneManagement.EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo()) {
-                UnityEditor.SceneManagement.EditorSceneManager.OpenScene(m_SceneToOpen);
-                //EditorApplication.isPlaying = true;
-            }
-            m_SceneToOpen = null;
-        }
-    }
-    #endregion
-#endif
-
-
-
     [CreateAssetMenu(fileName = "New SceneSwitcher", menuName = "UCL/SceneSwitcher", order = 0)]
     public class UCL_SceneSwitcher : ScriptableObject {
         [System.Serializable]
---
+++ UCL_SceneScript/UCL_EditorSceneLoader.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+namespace UCL.SceneLib {
+    #region Editor
+    public static class EditorSceneLoader {
+        static string m_SceneToOpen;
+        static bool m_Rigistered = false;
+        public static void LoadScene(string scene) {
+            if(UnityEditor.EditorApplication.isPlaying) {
+                UnityEditor.EditorApplication.isPlaying = false;
+            }
+
+            m_SceneToOpen = scene;
+            if(!m_Rigistered) {
+                UnityEditor.EditorApplication.update += OnUpdate;
+                m_Rigistered = true;
+            } else {
+                Debug.LogWarning("SceneHelper m_Rigistered!!");
+            }
+
+        }
+
+        static void OnUpdate() {
+            if(m_SceneToOpen == null) {
+                UnityEditor.EditorApplication.update -= OnUpdate;
+                m_Rigistered = false;
+                return;
+            }
+
+            if(UnityEditor.EditorApplication.isPlaying || UnityEditor.EditorApplication.isPaused ||
+                UnityEditor.EditorApplication.isCompiling || UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode) {
+                return;
+            }
+
+            UnityEditor.EditorApplication.update -= OnUpdate;
+            m_Rigistered = false;
+
+            if(UnityEditor.SceneManagement.EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo()) {
+                UnityEditor.SceneManagement.EditorSceneManager.OpenScene(m_SceneToOpen);
+                //EditorApplication.isPlaying = true;
+            }
+            m_SceneToOpen = null;
+        }
+    }
+    #endregion
+
+}
+#endif
