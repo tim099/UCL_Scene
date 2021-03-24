e7a0e5be407d19ffca8e4d48c8ab6649efb9649d
Tim Yeh <tim11251994@gmail.com>
2021-03-24 17:37:17 +0800

    Update UCL_EditorSceneLoader.cs

--- UCL_SceneScript/UCL_EditorSceneLoader.cs
+++ UCL_SceneScript/UCL_EditorSceneLoader.cs
@@ -7,44 +7,29 @@ using UCL.Core.EditorLib;
 
 namespace UCL.SceneLib {
     public static class EditorSceneLoader {
-        static string m_SceneToOpen;
-        static bool m_Rigistered = false;
 #if UNITY_EDITOR
-        public static void LoadScene(string scene) {
+        public static void LoadScene(string iSceneName) {
             if(UCL.Core.EditorLib.EditorApplicationMapper.isPlaying) {
                 UCL.Core.EditorLib.EditorApplicationMapper.isPlaying = false;
             }
 
-            m_SceneToOpen = scene;
-            if(!m_Rigistered) {
-                EditorApplicationMapper.update += OnUpdate;
-                m_Rigistered = true;
-            } else {
-                Debug.LogWarning("SceneHelper m_Rigistered!!");
-            }
-
+            UCL.Core.EditorLib.UCL_EditorUpdateManager.AddAction(()=>Load(iSceneName));
         }
 
-        static void OnUpdate() {
-            if(m_SceneToOpen == null) {
-                EditorApplicationMapper.update -= OnUpdate;
-                m_Rigistered = false;
+        static void Load(string iSceneName) {
+            if(string.IsNullOrEmpty(iSceneName)) {
                 return;
             }
 
             if(UCL.Core.EditorLib.EditorApplicationMapper.isPlaying || UCL.Core.EditorLib.EditorApplicationMapper.isPaused ||
                 UCL.Core.EditorLib.EditorApplicationMapper.isCompiling || UCL.Core.EditorLib.EditorApplicationMapper.isPlayingOrWillChangePlaymode) {
+                UCL.Core.EditorLib.UCL_EditorUpdateManager.AddAction(() => Load(iSceneName));//Wait 1 Frame
                 return;
             }
 
-            EditorApplicationMapper.update -= OnUpdate;
-            m_Rigistered = false;
-
             if(UnityEditor.SceneManagement.EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo()) {
-                UnityEditor.SceneManagement.EditorSceneManager.OpenScene(m_SceneToOpen);
-                //EditorApplication.isPlaying = true;
+                UnityEditor.SceneManagement.EditorSceneManager.OpenScene(iSceneName);
             }
-            m_SceneToOpen = null;
         }
 #endif
     }
